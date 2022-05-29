fdae186dbb8254dc26224d1b46551aaa17363e25
Fatih Çalış <93558245+fatihcaliss@users.noreply.github.com>
2022-05-29 22:17:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -38,6 +38,9 @@ function subtracting(item){
     if(payment.innerText == '$19.00'){ 
         shippingPrice.innerText = '$0.00';
         payment.innerText = '$0.00';
+    }else{
+        shippingPrice.innerText = '$0.00';
+        payment.innerText = '$0.00';
     }
 }}
 
